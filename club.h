822a0d2fba72aeb32a6d2b0c484e3eb1a07f71c3
mraehanakbar <43035458+mraehanakbar@users.noreply.github.com>
2019-04-18 06:05:36 +0700

    Update club.h

--- club.h
+++ club.h
@@ -1,4 +1,4 @@
-//
+/
 // Created by rsof on 14/04/19.
 //
 
@@ -19,11 +19,11 @@ void delete_last_club(list_club &S);
 void insert_after_club(list_club &S, address_club P, int R);
 void delete_after_club(list_club &S, int R);
 address_club search_club(list_club S,string nama_club);
-void update_club(address P,string club);
 void set_player_club(list_player L1,list_club L2,string nama_club,string nama_pemain);
 void delete_player_any_club(list_player L1,list_club L2,string player);
 void set_player_ex(list_player L1,list_club L2,string nama_club,string nama_pemain);
 void player_and_ex(list_player L1,list_club L2,string namaplayer);
 void search_the_most_player_have_ex(list_player L1,list_club L2);
+void list_club_on_list(list_club C);
 void the_worst_ex_club(list_club L2);
 #endif //TUBES_STD_CLUB_H
